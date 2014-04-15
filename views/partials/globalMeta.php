fbbf0c5654503e7acd8f0698c7a0e436ec1dace0
roluckey <rosalie@strategicpointmarketing.com>
2014-04-15 18:06:42 -0500

    Fixed Duplicate Title Tag Issue

--- views/partials/globalMeta.php
+++ views/partials/globalMeta.php
@@ -1,6 +1,5 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<title>Tradewind Distribution</title>
 	
 	<!-- Styles -->
 	<!--[if lt IE 9]><link href="/templates/leaf-base/resources/c/css/global-fixed.css" rel="stylesheet"><![endif]-->
